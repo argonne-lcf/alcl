f34edfbfd8f47cd4132263eb83c2b73b555f6a07
Brice Videau <bvideau@anl.gov>
2019-09-18 03:43:15 +0000

    Finished C++ examples.

--- C++/buffer.cpp
+++ C++/buffer.cpp
@@ -1,313 +1,69 @@
-// Includes
-#include <stdio.h>
-#include <CL/cl.h>
-
-//  _
-// |_)  _  |  _  ._ ._  |  _. _|_  _
-// |_) (_) | (/_ |  |_) | (_|  |_ (/_
-//                  |
-
-/* - - - -
-IO
-- - - - */
-int read_from_binary(unsigned char **output, size_t *size, const char *name) {
-  FILE *fp = fopen(name, "rb");
-  if (!fp) {
-    return -1;
-  }
-
-  fseek(fp, 0, SEEK_END);
-  *size = ftell(fp);
-  fseek(fp, 0, SEEK_SET);
-
-  *output = (unsigned char *)malloc(*size * sizeof(unsigned char));
-  if (!*output) {
-    fclose(fp);
-    return -1;
-  }
-
-  fread(*output, *size, 1, fp);
-  fclose(fp);
-  return 0;
-}
-
-/* - - - -
-OpenCL Error
-- - - - */
-
-const char *getErrorString(cl_int error)
-{
-switch(error){
-    // run-time and JIT compiler errors
-    case 0: return "CL_SUCCESS";
-    case -1: return "CL_DEVICE_NOT_FOUND";
-    case -2: return "CL_DEVICE_NOT_AVAILABLE";
-    case -3: return "CL_COMPILER_NOT_AVAILABLE";
-    case -4: return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
-    case -5: return "CL_OUT_OF_RESOURCES";
-    case -6: return "CL_OUT_OF_HOST_MEMORY";
-    case -7: return "CL_PROFILING_INFO_NOT_AVAILABLE";
-    case -8: return "CL_MEM_COPY_OVERLAP";
-    case -9: return "CL_IMAGE_FORMAT_MISMATCH";
-    case -10: return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
-    case -11: return "CL_BUILD_PROGRAM_FAILURE";
-    case -12: return "CL_MAP_FAILURE";
-    case -13: return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
-    case -14: return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
-    case -15: return "CL_COMPILE_PROGRAM_FAILURE";
-    case -16: return "CL_LINKER_NOT_AVAILABLE";
-    case -17: return "CL_LINK_PROGRAM_FAILURE";
-    case -18: return "CL_DEVICE_PARTITION_FAILED";
-    case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
-
-    // compile-time errors
-    case -30: return "CL_INVALID_VALUE";
-    case -31: return "CL_INVALID_DEVICE_TYPE";
-    case -32: return "CL_INVALID_PLATFORM";
-    case -33: return "CL_INVALID_DEVICE";
-    case -34: return "CL_INVALID_CONTEXT";
-    case -35: return "CL_INVALID_QUEUE_PROPERTIES";
-    case -36: return "CL_INVALID_COMMAND_QUEUE";
-    case -37: return "CL_INVALID_HOST_PTR";
-    case -38: return "CL_INVALID_MEM_OBJECT";
-    case -39: return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
-    case -40: return "CL_INVALID_IMAGE_SIZE";
-    case -41: return "CL_INVALID_SAMPLER";
-    case -42: return "CL_INVALID_BINARY";
-    case -43: return "CL_INVALID_BUILD_OPTIONS";
-    case -44: return "CL_INVALID_PROGRAM";
-    case -45: return "CL_INVALID_PROGRAM_EXECUTABLE";
-    case -46: return "CL_INVALID_KERNEL_NAME";
-    case -47: return "CL_INVALID_KERNEL_DEFINITION";
-    case -48: return "CL_INVALID_KERNEL";
-    case -49: return "CL_INVALID_ARG_INDEX";
-    case -50: return "CL_INVALID_ARG_VALUE";
-    case -51: return "CL_INVALID_ARG_SIZE";
-    case -52: return "CL_INVALID_KERNEL_ARGS";
-    case -53: return "CL_INVALID_WORK_DIMENSION";
-    case -54: return "CL_INVALID_WORK_GROUP_SIZE";
-    case -55: return "CL_INVALID_WORK_ITEM_SIZE";
-    case -56: return "CL_INVALID_GLOBAL_OFFSET";
-    case -57: return "CL_INVALID_EVENT_WAIT_LIST";
-    case -58: return "CL_INVALID_EVENT";
-    case -59: return "CL_INVALID_OPERATION";
-    case -60: return "CL_INVALID_GL_OBJECT";
-    case -61: return "CL_INVALID_BUFFER_SIZE";
-    case -62: return "CL_INVALID_MIP_LEVEL";
-    case -63: return "CL_INVALID_GLOBAL_WORK_SIZE";
-    case -64: return "CL_INVALID_PROPERTY";
-    case -65: return "CL_INVALID_IMAGE_DESCRIPTOR";
-    case -66: return "CL_INVALID_COMPILER_OPTIONS";
-    case -67: return "CL_INVALID_LINKER_OPTIONS";
-    case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
-    case -69:  return "CL_INVALID_PIPE_SIZE";
-    case -70: return "CL_INVALID_DEVICE_QUEUE";
-
-    default: return "Unknown OpenCL error";
- }
-}
-
-static void check_error(cl_int error, char const *name) {
-    if (error != CL_SUCCESS) {
-        fprintf(stderr, "Non-successful return code %d (%s) for %s.  Exiting.\n", error, getErrorString(error), name);
-        exit(EXIT_FAILURE);
-    }
-}
-
-static void exit_msg(char const *str){
-        fprintf(stderr, "%s \n", str);
-        exit(EXIT_FAILURE);
-}
-
-// =================================================================================================
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+#define CL_HPP_ENABLE_EXCEPTIONS 1
+#include <CL/cl2.hpp>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include "./clerror.h"
 
 int main(int argc, char* argv[]) {
 
-   cl_int err;
-    
-   if (argc < 5)
-        exit_msg("Not enought arguments.");
 
-   //  _              _                      _
-   // |_) |  _. _|_ _|_ _  ._ ._ _    ()    | \  _     o  _  _
-   // |   | (_|  |_  | (_) |  | | |   (_X   |_/ (/_ \/ | (_ (/_
-   //
-    printf(">>> Initializing OpenCL Platform and Device...\n");
-
-    cl_uint platform_idx = (cl_uint) atoi(argv[1]);
-    cl_uint device_idx = (cl_uint) atoi(argv[2]);
-    
-    char name[128];
-    /* - - - 
-    Plateform
-    - - - - */
-    //A platform is a specific OpenCL implementation, for instance AMD, NVIDIA or Intel.
-    // Intel may have a different OpenCL implementation for the CPU and GPU.
-
-    // Discover the number of platforms:
-    cl_uint platform_count;
-    err = clGetPlatformIDs(0, NULL, &platform_count);
-    check_error(err, "clGetPlatformIds");
-
-    // Now ask OpenCL for the platform IDs:
-    cl_platform_id* platforms = (cl_platform_id*)malloc(sizeof(cl_platform_id) * platform_count);
-    err = clGetPlatformIDs(platform_count, platforms, NULL);
-    check_error(err, "clGetPlatformIds");
-
-    cl_platform_id platform = platforms[platform_idx];
-    err = clGetPlatformInfo(platform, CL_PLATFORM_NAME, 128, name, NULL);
-    check_error(err, "clGetPlatformInfo");
-
-    printf("Platform #%d: %s\n", platform_idx, name);
-
-    /* - - - -
-    Device
-    - - - - */
-    // Device gather data
-    cl_uint device_count;
-    err = clGetDeviceIDs(platform,  CL_DEVICE_TYPE_ALL , 0, NULL, &device_count);
-    check_error(err, "clGetdeviceIds");
-
-    cl_device_id* devices = (cl_device_id*)malloc(sizeof(cl_device_id) * device_count);
-    err = clGetDeviceIDs(platform,  CL_DEVICE_TYPE_ALL , device_count, devices, NULL);
-    check_error(err, "clGetdeviceIds");
-
-    cl_device_id device = devices[device_idx];
-    err = clGetDeviceInfo(device, CL_DEVICE_NAME, 128, name, NULL);
-    check_error(err, "clGetPlatformInfo");
-
-    printf("-- Device #%d: %s\n", device_idx, name);
-    
-    //  _                               _                  
-    // /   _  ._ _|_  _    _|_   ()    / \      _       _  
-    // \_ (_) | | |_ (/_ >< |_   (_X   \_X |_| (/_ |_| (/_ 
-    //                                                     
-
-    /* - - - -
-    Context
-    - - - - */
-    // A context is a platform with a set of available devices for that platform.
-    cl_context context = clCreateContext(0, device_count, devices, NULL, NULL, &err);
-    check_error(err,"clCreateContext");
-
-    /* - - - -
-    Command queue
-    - - - - */
-    // The OpenCL functions that are submitted to a command-queue are enqueued in the order the calls are made but can be configured to execute in-order or out-of-order.
-    const cl_queue_properties properties[] =  { CL_QUEUE_PROPERTIES, (CL_QUEUE_PROFILING_ENABLE), 0 };
-
-    cl_command_queue queue = clCreateCommandQueueWithProperties(context, device, properties, &err);
-    check_error(err,"clCreateCommandQueueWithProperties");
-
-    //  _       _   _
-    // |_)    _|_ _|_ _  ._
-    // |_) |_| |   | (/_ |
-
-    // Length of vectors
-    size_t n =  {   (size_t) atoi(argv[3]) };
-    size_t bytes = n*sizeof(double);
-    double *h_a = (double*)malloc(bytes);
-    cl_mem d_a = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, bytes, NULL, &err);
-    check_error(err,"cclCreateBuffer");
-
-    // |/  _  ._ ._   _  |
-    // |\ (/_ |  | | (/_ |
-    //
-    printf(">>> Kernel configuration...\n");
-
-    // String kernel.
-    // Use MPI terminology (we are HPC!)
-    const char *kernelstring =
-        "__kernel void hello_world(__global double *a, const unsigned int n) {"
-        "   const int world_rank = get_global_id(0);"
-        "    if (world_rank < n)"
-        "    a[world_rank] =  world_rank;"
-        "}";
-
-    // Create the program
-    cl_program program = clCreateProgramWithSource(context, 1, &kernelstring, NULL, &err);
-    check_error(err,"clCreateProgramWithSource");
-
-    //Build / Compile the program executable
-    err = clBuildProgram(program, device_count, devices, "-cl-unsafe-math-optimizations", NULL, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to build program executable!\n");
-
-        size_t logSize;
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &logSize);
-
-        // there's no information in the reference whether the string is 0 terminated or not.
-        char* messages = (char*)malloc((1+logSize)*sizeof(char));
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize, messages, NULL);
-        messages[logSize] = '\0';
-
-        printf("%s", messages);
-        free(messages);
-        return EXIT_FAILURE;
+    if (argc < 5) {
+        std::cerr << argv[0] << " platform_id device_id global_size local_size" << std::endl;
+        exit(1);
     }
 
-   /* - - - -
-    Create
-    - - - - */
-    cl_kernel kernel = clCreateKernel(program, "hello_world", &err);
-    check_error(err,"clCreateKernel");
+    int platform_idx = atoi(argv[1]);
+    int device_idx = atoi(argv[2]);
+    int global_size = atoi(argv[3]);
+    int local_size = atoi(argv[4]);
 
-    err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
-    err |= clSetKernelArg(kernel, 1, sizeof(unsigned int), &n);
-    check_error(err,"clSetKernelArg");
+    try {
+        std::vector<cl::Platform> platforms;
+        cl::Platform::get(&platforms);
 
-    /* - - - -
-    ND range
-    - - - - */
-    printf(">>> NDrange configuration...\n");
+        cl::Platform plat = platforms[platform_idx];
+        std::cout << "Platform: " << plat.getInfo<CL_PLATFORM_NAME>() << std::endl;
 
-    #define WORK_DIM 1
+        std::vector<cl::Device> devices;
+        plat.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+        std::cout << "  -- Device: " << devices[device_idx].getInfo<CL_DEVICE_NAME>() << std::endl;
 
-    // Describe the number of global work-items in work_dim dimensions that will execute the kernel function
-    size_t global0 = (size_t) atoi(argv[3]);
-    const size_t global[WORK_DIM] = {  global0 };
+        cl::Context context(devices);
 
-    // Describe the number of work-items that make up a work-group (also referred to as the size of the work-group).
-    // local_work_size can also be a NULL value in which case the OpenCL implementation will determine how to be break the global work-items into appropriate work-group instances.
-    size_t local0 = (size_t) atoi(argv[4]);
-    const size_t local[WORK_DIM] = { local0 };
+        cl::CommandQueue queue(context, devices[device_idx]);
 
-    printf("Global work size: %zu \n", global[0]);
-    printf("Local work size: %zu \n", local[0]);
+        std::string kernelstring{R"CLC(
+        __kernel void hello_world(__global double *a, const unsigned int n) {
+            const int world_rank = get_global_id(0);
+            if (world_rank < n)
+            a[world_rank] =  world_rank;
+        }
+        )CLC"};
 
-    /* - - - -
-    Execute
-    - - - - */
-    printf(">>> Kernel Execution...\n");
+        cl::Program program(context, kernelstring);
+        program.build("-cl-unsafe-math-optimizations");
 
-    err  = clEnqueueNDRangeKernel(queue, kernel, WORK_DIM, NULL, global, local, 0, NULL, NULL);
-    check_error(err,"clEnqueueNDRangeKernel");
+        cl::Kernel kernel(program, "hello_world");
 
-    /* - - - 
-    Sync & check
-    - - - */
+        auto kernel_func = cl::KernelFunctor<cl::Buffer, int>(kernel);
 
-    // Wait for the command queue to get serviced before reading back results
-    clFinish(queue);
+        std::vector<double> h_a(global_size);
+        cl::Buffer d_a(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, global_size*sizeof(double));
 
-    // Read the results from the device
-    clEnqueueReadBuffer(queue, d_a, CL_TRUE, 0, bytes, h_a, 0, NULL, NULL );
-    double sum = 0;
-    for(int i=0; i<n; i++)
-        sum += h_a[i];
-     printf("final result: %f, should have been %lu\n", sum, n*(n-1)/2);
+        kernel_func(cl::EnqueueArgs(queue, cl::NDRange(global_size), cl::NDRange(local_size)), d_a, global_size);
+        queue.enqueueReadBuffer(d_a, false, 0, global_size*sizeof(double), h_a.data());
+        queue.finish();
 
-    //  _                         
-    // /  |  _   _. ._  o ._   _  
-    // \_ | (/_ (_| | | | | | (_| 
-    //                         _| 
-    clReleaseCommandQueue(queue);
-    clReleaseContext(context);
-    clReleaseProgram(program);
-    clReleaseKernel(kernel);
+        double sum = std::accumulate(h_a.begin(), h_a.end(), 0.0);
+        std::cout << "Final result: " << sum << ", should have been " << global_size*(global_size-1)/2 << std::endl;
 
-    // Exit
+    } catch (cl::Error e) {
+        std::cout << e.what()<< "(" << e.err() << ": " << getErrorString(e.err()) << ")" << std::endl;
+        exit(1);
+    }
     return 0;
 }
 
